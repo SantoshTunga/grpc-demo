c527379c07f743b887e8d672b953d19a1f5aa962
Santosh Tunga <santosh2tunga@gmail.com>
2026-09-11 12:27:34 +0530

    change in proto file

--- src/main/java/com/grpc/demo/service/ProductService.java
+++ src/main/java/com/grpc/demo/service/ProductService.java
@@ -15,7 +15,7 @@ public class ProductService extends ProductServiceGrpc.ProductServiceImplBase {
     @Override
     public void getProductsByType(ProductRequestById request, StreamObserver<Product> responseObserver) {
 
-        Integer productTypeId = request.getTypeId();
+        String productTypeId = request.getTypeId();
 
         // Flux with Response Observer is used .
 
@@ -23,7 +23,7 @@ public class ProductService extends ProductServiceGrpc.ProductServiceImplBase {
         Flux.interval(Duration.ofMillis(100)).take(100)
                 .map(i -> Product.newBuilder().setId("P-" + i).setTitle("Product Item #" + i)
                         .setPrice(49.99 * i)
-                        .setTypeId(String.valueOf(productTypeId))
+                        .setTypeId(productTypeId)
                         .setStockQuantity(10 * i)
                         .build())
                 .subscribe(
