f1205339c13625384b8386b0de335c897bc7d122
Santosh Tunga <santosh2tunga@gmail.com>
2026-09-12 04:39:43 +0530

    rest vs grpc comment

--- src/main/java/com/grpc/demo/GrpcDemoApplication.java
+++ src/main/java/com/grpc/demo/GrpcDemoApplication.java
@@ -6,8 +6,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GrpcDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GrpcDemoApplication.class, args);
-	}
+    static void main(String[] args) {
+        SpringApplication.run(GrpcDemoApplication.class, args);
+    }
 
 }
+
+/**
+ *
+ * | REST                        | gRPC                         |
+ * | --------------------------- | ---------------------------- |
+ * | `200 OK`                    | `OK`                         |
+ * | `400 Bad Request`           | `INVALID_ARGUMENT`           |
+ * | `401 Unauthorized`          | `UNAUTHENTICATED`            |
+ * | `403 Forbidden`             | `PERMISSION_DENIED`          |
+ * | `404 Not Found`             | `NOT_FOUND`                  |
+ * | `409 Conflict`              | `ALREADY_EXISTS` / `ABORTED` |
+ * | `429 Too Many Requests`     | `RESOURCE_EXHAUSTED`         |
+ * | `500 Internal Server Error` | `INTERNAL`                   |
+ * | `503 Service Unavailable`   | `UNAVAILABLE`                |
+ * | `504 Gateway Timeout`       | `DEADLINE_EXCEEDED`          |
+ */
